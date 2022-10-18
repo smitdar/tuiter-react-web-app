6bde855cff3d7176591c80341685a4a1f0950907
smitdar21 <smit@pointfocal.com>
2022-10-18 18:14:07 -0400

    A5 final changes

--- public/tuiter/HomeScreen/Home.js
+++ public/tuiter/HomeScreen/Home.js
@@ -1,32 +1,54 @@
-import React from "react";
-import {Link} from "react-router-dom";
-import NavigationSidebar from '../NavigationSidebar';
-import HomeScreen from "../HomeScreen";
-import PostSummaryList from '../PostSummaryList';
+//import React from "react";
+//import {Link} from "react-router-dom";
+//import NavigationSidebar from '../NavigationSidebar';
+//import HomeScreen from "../HomeScreen";
+//import PostSummaryList from '../PostSummaryList';
+import NavigationSidebar from "../NavigationSidebar/index.js";
+import ExploreComponent from "../ExploreComponent/index.js";
+import HomeScreen from "./index.js";
 
-const Home = () => {
-    return(
-        <div>
-            <div className="row pt-3">
-                <div className="col-3 col-lg-2 col-xl-2">
-                    <NavigationSidebar active="home"/>
-                </div>
-                <div className="col-9 col-lg-6 col-xl-6">
-                    <HomeScreen/>
-                </div>
-                <div className="col-lg-4 col-xl-4 d-none d-lg-block ps-4">
-                    <PostSummaryList/>
-                </div>
-            </div>
 
-            <Link to="/a6/hello">
-                Hello
-            </Link> |
-            <Link to="/a6/practice">
-                Practice
-            </Link>
-        </div>
-    )
-}
+(function ($) {
+    $('#wd-home').append(` <div class="row mt-2">
+                                             <div class="col-2 col-md-2 col-lg-1 col-xl-2">
+                                             ${NavigationSidebar()}
+                                             </div>
+                                             <div class="col-10 col-md-10 col-lg-7 col-xl-6">
+                                                 ${HomeScreen()}
+                                             </div>
+                                             <div class="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4 wd-ps-col3">
+                                                 <div class="wd-right-section">
+                                                     ${ExploreComponent()}
+                                                     </div>
+                                             </div>
+                                         </div>
+                                     `);
+                             })($);
 
-export default Home;
+
+//const Home = () => {
+//    return(
+//        <div>
+//            <div className="row pt-3">
+//                <div className="col-3 col-lg-2 col-xl-2">
+//                    <NavigationSidebar active="home"/>
+//                </div>
+//                <div className="col-9 col-lg-6 col-xl-6">
+//                    <HomeScreen/>
+//                </div>
+//                <div className="col-lg-4 col-xl-4 d-none d-lg-block ps-4">
+//                    <PostSummaryList/>
+//                </div>
+//            </div>
+//
+//            <Link to="/a6/hello">
+//                Hello
+//            </Link> |
+//            <Link to="/a6/practice">
+//                Practice
+//            </Link>
+//        </div>
+//    )
+//}
+//
+//export default Home;
--- public/tuiter/HomeScreen/index.js
+++ public/tuiter/HomeScreen/index.js
@@ -1,19 +1,60 @@
-import PostItem from "./PostItem";
-import post from "./post";
-import './index.css';
+import PostItem from "./PostItem.js";
+import Homepost from "./Homepost.js";
+
+
+
+//const HomeScreen = () => {
+//                             return `
+//                              <div>
+//                                      ${Homepost.map(posts => {
+//                                       return (PostItem(posts));
+//                                      }).join('')}
+//                                     </div>
+//                                      `
+//                                };
+
+
+
+
+//export default HomeScreen;
+
+/////
 
 const HomeScreen = () => {
-    return(
-        <div>
-            <ul className="list-group wd-font-size">
-                {
-                post.map(p => {
-                    return(<PostItem userName={p.userName} handle={p.handle} time={p.time} tagline={p.tagline} profileImage={p.profileImage} descriptionImage={p.descriptionImage} topic={p.topic} description={p.description} link={p.link} linkText={p.linkText} commentsCount={p.commentsCount} retweetsCount={p.retweetsCount} likesCount={p.likesCount}/>)
-                })
-            }
-            </ul>
-        </div>
-    );
-}
 
+    return (`
+
+            <ul class="list-group">
+
+            <!-- continue here -->
+
+            ${Homepost.map(post => {
+
+        return (PostItem(post));
+
+    }).join('')
+
+        }
+
+            </ul>
+
+`);
+
+}
 export default HomeScreen;
+
+//const HomeScreen = () => {
+//    return(`
+//        <div>
+//            <ul className="list-group wd-font-size">
+//                {
+//                Homepost.map(p => {
+//                    return(<PostItem userName={p.userName} handle={p.handle} time={p.time} tagline={p.tagline} profileImage={p.profileImage} descriptionImage={p.descriptionImage} topic={p.topic} description={p.description} link={p.link} linkText={p.linkText} commentsCount={p.commentsCount} retweetsCount={p.retweetsCount} likesCount={p.likesCount}/>)
+//                })
+//            }
+//            </ul>
+//        </div>
+//    `);
+//}
+//
+//export default HomeScreen;
