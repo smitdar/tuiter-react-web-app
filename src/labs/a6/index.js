d5b416dd637b5a74269f783d3b44ca76d2a729c2
smitdar21 <smit@pointfocal.com>
2022-10-20 14:45:48 -0400

    A6 lab done

---
+++ src/labs/a6/index.js
@@ -0,0 +1,18 @@
+import Classes from "./classes/index.js";
+import Styles from "./styles";
+import ConditionalOutput
+  from "./conditional-output/index.js";
+import Todo from "./todo/todo-list.js"
+
+function Assignment6() {
+ return (
+   <div>
+     <h1>Assignment 6</h1>
+     <ConditionalOutput/>
+     <Styles/>
+     <Classes/>
+     <Todo/>
+   </div>
+ );
+}
+export default Assignment6;
