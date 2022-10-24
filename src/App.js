db9597df0f2309517eedd3dbb740d46faf7f75d2
smitdar21 <smit@pointfocal.com>
2022-10-23 21:24:09 -0400

    A6 done

--- src/App.js
+++ src/App.js
@@ -9,9 +9,12 @@ function App() {
     <BrowserRouter>
        <div className="container">
            <Routes>
-                  <Route index element={<Labs/>}/>
-                  <Route path="/hello" element={<HelloWorld/>}/>
-                  <Route path="/tuiter/*" element={<Tuiter/>}/>
+               <Route index
+                      element={<Labs/>}/>
+               <Route path="/hello"
+                      element={<HelloWorld/>}/>
+               <Route path="/tuiter/*"
+                      element={<Tuiter/>}/>
            </Routes>
        </div>
     </BrowserRouter>
