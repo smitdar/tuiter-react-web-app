54f25933c5929b7a484fe25f51002ece4908a22a
smitdar21 <smit@pointfocal.com>
2022-10-25 08:47:35 -0400

    A6 done

--- src/tuiter/home/HomeComponent.js
+++ src/tuiter/home/HomeComponent.js
@@ -8,42 +8,42 @@ const HomeComponent = (
 
 <div>
 
-    <div class="wd-flex-div">
+    <div className="wd-flex-div">
 
 
-        <div class="wd-paddingtop">
-            <img class="wd-profile-image" src={home.profileImage}></img>
+        <div className="wd-paddingtop">
+            <img className="wd-profile-image" src={home.profileImage}></img>
         </div>
-        <div class="wd-floattop">
-            <h1 class="wd-profilename">{home.userName} </h1>
-            <h2 class="wd-handlename"> @{home.handle} - {home.time}<br/></h2>
+        <div className="wd-floattop">
+            <h1 className="wd-profilename">{home.userName} </h1>
+            <h2 className="wd-handlename"> @{home.handle} - {home.time}<br/></h2>
 
             <br/>
-            <p class="wd-text-content">
+            <p className="wd-text-content">
                 {home.tagline}
 
             </p>
         </div>
     </div>
 
-    <div class="wd-content-border">
+    <div className="wd-content-border">
 
-        <img class="wd-content-img" src={home.descriptionImage}></img>
+        <img className="wd-content-img" src={home.descriptionImage}></img>
 
         <br/>
-        <h1 class="wd-belowtext-title">
+        <h1 className="wd-belowtext-title">
             {home.topic}
         </h1>
-        <h2 class="wd-belowtext">
+        <h2 className="wd-belowtext">
             {home.description}
             </h2>
     </div>
 
     <div>
-        <a class="wd-bottom-div" href="#">&#128495; 50</a>
-        <a class="wd-bottom-div" href="#">&#8634; 10</a>
-        <a class="wd-heart" href="#">&#9829; 22</a>
-        <a class="wd-bottom-div" href="#">&#8613;</a>
+        <a className="wd-bottom-div" href="#">&#128495; 50</a>
+        <a className="wd-bottom-div" href="#">&#8634; 10</a>
+        <a className="wd-heart" href="#">&#9829; 22</a>
+        <a className="wd-bottom-div" href="#">&#8613;</a>
 
     </div>
     <br/>
